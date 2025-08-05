1cf2ab40c1c1f89f46648d5866e46efad41800d3
Ghostdee <thomaskuwaikwan@gmail.com>
2025-08-05 17:46:05 +0800

    added comparison and forecast functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from datetime import datetime
+from collections import Counter
+from datetime import datetime, timedelta
 
 import requests
 from decouple import config
@@ -15,7 +16,8 @@ WEATHER_FORECAST = "f"
 WEATHER_COMPARISON = "c"
 
 console = Console()
-current_day = datetime.now()
+CURRENT_DAY = datetime.now()
+six_days_list = []
 FUNCTIONS = [
     "Weather Description [bold blue](d)[/]",
     "Weather Forecast [bold blue](f)[/]",
@@ -64,10 +66,20 @@ SPECIFIC_WIND_DIRECTIONS = {
     "WNW": (270, 315),
     "NNW": (315, 360),
 }
-COMPARISON_LIST = []
+COMPARISON_LIST = ["Weather", "Temperature"]
+WEATHER = "1"
+TEMPERATURE = "2"
 
 city_list = []
 checked_cities = []
+
+
+def from_kelvin_convert_to_celsius(temperature: float) -> float:
+    return temperature - 273.15
+
+
+def from_celsius_convert_to_fahrenheit(temperature: float) -> float:
+    return temperature * 9 / 5 + 32
 
 
 def api_call(city_name: str):
@@ -77,8 +89,9 @@ def api_call(city_name: str):
                 BASE_URL=BASE_URL, city_name=city_name, API_KEY=API_KEY
             )
         ).json()
-    except TimeoutError:
+    except requests.exceptions.ConnectTimeout:
         console.print("[bold red]Unable to connect. Please try again later.[/]")
+        return None
 
     if response["cod"] == "404":
         error_message = f"[bold red]{response['message'].capitalize()}[/]"
@@ -105,7 +118,7 @@ def get_weather_descriptions(response) -> dict:
     return {
         "weather_status": response["weather"][0]["main"],
         "weather_description": response["weather"][0]["description"].capitalize(),
-        "temperature_celsius": response["main"]["temp"] - 273.15,
+        "temperature_celsius": from_kelvin_convert_to_celsius(response["main"]["temp"]),
         "humidity": response["main"]["humidity"],
         "wind_speed": response["wind"]["speed"],
         "wind_direction": response["wind"]["deg"],
@@ -119,10 +132,11 @@ def print_weather_descriptions(response, city_name: str, unit_preference: str) -
     console.print(
         f"{city_name.title()} is {WEATHERS[weather_descriptions["weather_status"]]} today. ({weather_descriptions["weather_description"]})"
     )
-    if unit_preference == "f" or unit_preference == "2":
-        temperature_fahrenheit = (
-            9 / 5 * float(weather_descriptions["temperature_celsius"]) + 32
+    if unit_preference in ("2", "f"):
+        temperature_fahrenheit = from_celsius_convert_to_fahrenheit(
+            float(weather_descriptions["temperature_celsius"])
         )
+
         console.print(f"Temperature: {temperature_fahrenheit:.2f}[bold cyan]°F[/]")
     else:
         console.print(
@@ -136,7 +150,8 @@ def print_weather_descriptions(response, city_name: str, unit_preference: str) -
     console.print()
 
 
-def get_unit_preference():
+def get_unit_preference() -> str:
+    """Ask user for unit preference: f: fahrenheit anything else: celsius"""
     console.print("Which unit system do you prefer")
     for index, unit in enumerate(UNITS, start=1):
         console.print(f"{index}. {unit}")
@@ -145,8 +160,28 @@ def get_unit_preference():
 
 def check_city_weather(city_name: str, response) -> None:
     unit_preference = get_unit_preference()
-    console.print_weather_descriptions(response, city_name, unit_preference)
+    print_weather_descriptions(response, city_name, unit_preference)
     console.rule()
+
+
+def get_six_days_for_forecast():
+    for i in range(0, 6):
+        six_days_list.append(str(CURRENT_DAY + timedelta(days=i))[:10])
+
+
+first_day_temperature = []
+first_day_forecast_weather_count = Counter()
+second_day_temperature = []
+second_day_forecast_weather_count = Counter()
+third_day_temperature = []
+third_day_forecast_weather_count = Counter()
+fourth_day_temperature = []
+fourth_day_forecast_weather_count = Counter()
+fifth_day_temperature = []
+fifth_day_forecast_weather_count = Counter()
+sixth_day_temperature = []
+sixth_day_forecast_weather_count = Counter()
+temperature_and_weather_forecast = []
 
 
 def check_weather_forecast(response):
@@ -158,53 +193,170 @@ def check_weather_forecast(response):
             API_KEY=API_KEY,
         )
     ).json()
-    console.print(forecast_response)
+    get_six_days_for_forecast()
+    for forecast_info in forecast_response["list"]:
+        if forecast_info["dt_txt"][:10] == six_days_list[0]:
+            first_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            first_day_temperature.append(forecast_info["main"]["temp"])
+        elif forecast_info["dt_txt"][:10] == six_days_list[1]:
+            second_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            second_day_temperature.append(forecast_info["main"]["temp"])
+        elif forecast_info["dt_txt"][:10] == six_days_list[2]:
+            third_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            third_day_temperature.append(forecast_info["main"]["temp"])
+        elif forecast_info["dt_txt"][:10] == six_days_list[3]:
+            fourth_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            fourth_day_temperature.append(forecast_info["main"]["temp"])
+        elif forecast_info["dt_txt"][:10] == six_days_list[4]:
+            fifth_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            fifth_day_temperature.append(forecast_info["main"]["temp"])
+        elif forecast_info["dt_txt"][:10] == six_days_list[5]:
+            sixth_day_forecast_weather_count.update(
+                [forecast_info["weather"][0]["main"]]
+            )
+            sixth_day_temperature.append(forecast_info["main"]["temp"])
+    temperature_and_weather_forecast.append(
+        (first_day_temperature, first_day_forecast_weather_count)
+    )
+    temperature_and_weather_forecast.append(
+        (second_day_temperature, second_day_forecast_weather_count)
+    )
+    temperature_and_weather_forecast.append(
+        (third_day_temperature, third_day_forecast_weather_count)
+    )
+    temperature_and_weather_forecast.append(
+        (fourth_day_temperature, fourth_day_forecast_weather_count)
+    )
+    temperature_and_weather_forecast.append(
+        (fifth_day_temperature, fifth_day_forecast_weather_count)
+    )
+    temperature_and_weather_forecast.append(
+        (sixth_day_temperature, sixth_day_forecast_weather_count)
+    )
+
+    unit_preference = get_unit_preference()
+    console.print()
+    for day_index, (temperatures_of_the_day, weather_counts_of_the_day) in enumerate(
+        temperature_and_weather_forecast
+    ):
+        console.print(f"[{six_days_list[day_index]}]")
+        if weather_counts_of_the_day.most_common(1)[0] == "Tornado":
+            console.print(
+                "[bold red]The city is likely to be hit by a tornado! Please stay safe![/]"
+            )
+        else:
+            console.print(
+                f"The weather on this day is mostly {WEATHERS[weather_counts_of_the_day.most_common(1)[0][0]]}."
+            )
+        average_temperature = from_kelvin_convert_to_celsius(
+            sum(temperatures_of_the_day) / len(temperatures_of_the_day)
+        )
+        if unit_preference in ("2", "f"):
+            unit = "°F"
+            average_temperature = from_celsius_convert_to_fahrenheit(
+                average_temperature
+            )
+        else:
+            unit = "°C"
+        console.print(
+            f"The average temperature will be {average_temperature:.2f}{unit}."
+        )
+        console.print()
+    console.input("[blue]Press enter to continue.[/]")
+    console.print()
+    console.rule()
 
 
 def weather_comparison(city_name: str, response):
-    first_city_name = city_name
+    first_city_name = city_name.title()
     first_city_info = get_weather_descriptions(response)
     while True:
         second_city_name = console.input(
-            "Please input another city name: [bold red](q to quit)[/] "
-        )
-        if second_city_name == "q":
+            "Please input another city name for comparison: [bold red](q to quit)[/]\n"
+        ).title()
+        if second_city_name == "Q":
             break
-        if second_city_name is not None:
-            second_response = api_call(second_city_name)
-            second_city_info = get_weather_descriptions(second_response)
-            console.print("Which information do you want to compare?")
-            # TODO: create the comparison list and print out a list for user to choose
-            for index, (first_city_info, second_city_info) in enumerate(
-                COMPARISON_LIST
-            ):
-                print(
-                    f"{first_city_name},{first_city_info},{second_city_name},{second_city_info}"
-                )
+
+        second_response = api_call(second_city_name)
+        if second_response is not None:
+            add_to_checked_city_list(second_city_name)
+            while True:
+                second_city_info = get_weather_descriptions(second_response)
+                console.print("Which information do you want to compare?")
+                # TODO: create the comparison list and print out a list for user to choose
+                for index, info_to_compare in enumerate(COMPARISON_LIST, start=1):
+                    console.print(f"{index}. {info_to_compare}")
+                info = console.input().strip()
+                if info == WEATHER:
+                    console.print(
+                        f"{first_city_name} is {WEATHERS[first_city_info["weather_status"]]}, while {second_city_name} is {WEATHERS[second_city_info["weather_status"]]}."
+                    )
+                    break
+                elif info == TEMPERATURE:
+                    console.print()
+                    unit_preference = get_unit_preference()
+                    console.print()
+                    first_city_temp = first_city_info["temperature_celsius"]
+                    second_city_temp = second_city_info["temperature_celsius"]
+                    difference = first_city_temp - second_city_temp
+                    if unit_preference in ("2", "f"):
+                        difference *= 9 / 5
+                        unit = "°F"
+                    else:
+                        unit = "°C"
+                    if difference < 0:
+                        console.print(
+                            f"{first_city_name} is {abs(difference):.2f} {unit} colder than {second_city_name}."
+                        )
+                    elif difference > 0:
+                        console.print(
+                            f"{first_city_name} is {difference:.2f} {unit} colder than {second_city_name}."
+                        )
+                    else:
+                        console.print(
+                            f"{first_city_name} has the same temperaure as {second_city_name}."
+                        )
+                    break
+                else:
+                    console.print("[bold red]Invalid input.[/]")
+        console.input("[blue]Press enter to continue.[/]")
+        console.print()
+        console.rule()
+        break
 
 
 def function_select(city_name: str, response) -> None:
     """Select from functions"""
     function_choice = None
-    while function_choice not in ("q", 4):
+    while function_choice not in ("q", "4"):
         console.print("Select a function:")
         for number, function in enumerate(FUNCTIONS, start=1):
             console.print(f"{number}. {function}")
             console.print()
-        function_choice = input().strip().lower()[:1]
+        function_choice = input().strip().lower()
         console.print()
         console.rule()
-        if function_choice in (1, WEATHER_DESCRIPTION):
+        if function_choice in ("1", WEATHER_DESCRIPTION):
             check_city_weather(city_name, response)
-        elif function_choice in (2, WEATHER_FORECAST):
+        elif function_choice in ("2", WEATHER_FORECAST):
             check_weather_forecast(response)
-        elif function_choice in (3, WEATHER_COMPARISON):
+        elif function_choice in ("3", WEATHER_COMPARISON):
             weather_comparison(city_name, response)
 
 
 def get_all_cities() -> None:
     """store all city name into a list"""
-    with open("cities.json", "r", encoding="utf-8") as jsonfile:
+    with open(".cities.json", "r", encoding="utf-8") as jsonfile:
         data = json.load(jsonfile)
     for city in data:
         city_list.append(city["name"])
@@ -233,4 +385,6 @@ def main():
 
 
 if __name__ == "__main__":
+    get_six_days_for_forecast()
+    print(six_days_list)
     main()
